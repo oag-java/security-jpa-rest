0bf40f044b67125ed2c2454cd82863e5f4555443
otajon <otajonazamov20@gmail.com>
2021-07-09 09:41:00 +0500

    1

---
+++ security-jpa-rest/src/main/java/org/oag/securityjparest/SecurityJpaRestApplication.java
@@ -0,0 +1,13 @@
+package org.oag.securityjparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJpaRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJpaRestApplication.class, args);
+    }
+
+}
